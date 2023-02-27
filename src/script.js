1f491b0c7a1a25ef1c97edde037781cfca8ce7b1
Iara <iarapou21@hotmail.com>
2023-02-27 11:28:05 -0300

    Reemplaza código Jquery por un fetch()

--- src/script.js
+++ src/script.js
@@ -1,20 +1,18 @@
-function cargarCiudad(ciudad){
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + ciudad +"&appid=95176c8edea30e33338e0eaddd53a916", function(data){
-         console.log(data);
- 
-         document.querySelector("#ciudad").textContent = data.name;
-         document.querySelector("#temperatura").textContent = (data.main.temp - 273.15).toFixed(1);
+function mostrarCiudad(ciudad){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=95176c8edea30e33338e0eaddd53a916`)
+     .then(respuesta => respuesta.json())
+     .then(respuesta => {
+        console.log(respuesta)
+         document.querySelector("#ciudad").textContent = respuesta.name;
+         document.querySelector("#temperatura").textContent = (respuesta.main.temp - 273.15).toFixed(1);
          document.querySelector("#grados").innerHTML = "<sup>°C</sup>";
-         document.querySelector("#wicon").src="http://openweathermap.org/img/wn/"+ data.weather[0].icon +"@2x.png";
-         document.querySelector("#descripcion").textContent = data.weather[0].description;
- 
-         document.querySelector(".container").style.visibility = "visible";  
-     })
- 
-     .fail(function() {
-         alert(`Disculpá, no hayamos la ciudad ${ciudad}.`);
-     })
- }
+         document.querySelector("#wicon").src=`http://openweathermap.org/img/wn/${respuesta.weather[0].icon}@2x.png`;         
+         document.querySelector("#descripcion").textContent = respuesta.weather[0].description;
+         document.querySelector(".container").style.visibility = "visible";
+    })
+     .catch( respuesta => {alert(`Disculpá, no hayamos la ciudad ${ciudad}.`)})
+}
+
  
  let boton= document.querySelector("button");
  boton.addEventListener("click", function(){
@@ -25,7 +23,7 @@ function cargarCiudad(ciudad){
          alert("No ingresaste ningún valor.")
      } else {
      input.value = "";
-     cargarCiudad(ciudad);    
+     mostrarCiudad(ciudad);    
      }
  
      //Agregá una validación para que le muestre un alert al usuario si ingresa el nombre de una ciudad inexistente. Si la ciudad es inexistente, la API devolverá un Error 404. 🔍 Para aprender más, usá tus habilidades de developer y googleá la solución .fail.
